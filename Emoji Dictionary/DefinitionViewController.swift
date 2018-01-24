5e6db290aa44d4deff22adebfcd8056db4c8eae1
Mark Chan <lok.423@hotmail.com>
2018-01-24 13:24:08 +1300

    DefinitonViewController

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Mark Chan on 24/1/2018.
+//  Copyright © 2018年 Mark Chan. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
